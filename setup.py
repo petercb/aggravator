afbfdd9a7f3ebf9986ca0950152afa2096430480
Peter Burns <petercb@users.noreply.github.com>
2018-04-18 14:15:47 -0400

    Bumped version to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aggravator',
-    version='0.4.0',
+    version='0.4.1',
     description='Ansible inventory script to aggregate other inventory sources',
     long_description=open('README.rst').read(),
     license='MIT',
